fc0271fcbe954d7e041eb82aa9a10f0eae8357f6
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-04-30 14:39:29 +0200

    add count to version

--- src/main/java/com/dynatrace/ratings/controller/VersionController.java
+++ src/main/java/com/dynatrace/ratings/controller/VersionController.java
@@ -1,6 +1,8 @@
 package com.dynatrace.ratings.controller;
 
 import com.dynatrace.ratings.model.Version;
+import com.dynatrace.ratings.repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/version")
 public class VersionController {
+    @Autowired
+    private RatingRepository ratingRepository;
     @Value("${service.version}")
     private String svcVer;
     @Value("${service.date}")
@@ -17,6 +21,6 @@ public class VersionController {
 
     @GetMapping("")
     public Version getVersion() {
-        return new Version("ratings", svcVer, svcDate, "OK", "Healthy");
+        return new Version("ratings", svcVer, svcDate, "OK", "Count: " + ratingRepository.count());
     }
 }
